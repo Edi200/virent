ef07e155865677c1c9ea0426046954c4998d0b1f
Edvard Supica <edvard.supica@gmail.com>
2026-06-23 16:07:04 +0200

    feat: Unbounded font for wordmark, monochrome dark-mode treatment

--- resources/js/components/ViRentWordmark.vue
+++ resources/js/components/ViRentWordmark.vue
@@ -15,7 +15,7 @@ const props = withDefaults(
 );
 
 const wordmarkClass = cn(
-    'font-heading text-3xl font-bold tracking-[0.02em] uppercase sm:text-4xl',
+    'font-wordmark text-3xl font-bold tracking-[0.02em] uppercase sm:text-4xl',
     props.class,
 );
 </script>
@@ -27,11 +27,11 @@ const wordmarkClass = cn(
         aria-label="ViRent"
         :class="wordmarkClass"
     >
-        <span class="text-[#5E244E] dark:text-[#C97FAE]">Vi</span
-        ><span class="text-[#E68457]">Rent</span>
+        <span class="text-[#5E244E] dark:text-[#F3EEF5]">Vi</span
+        ><span class="text-[#E68457] dark:text-[#F3EEF5]">Rent</span>
     </Link>
     <span v-else :class="wordmarkClass" aria-hidden="true">
-        <span class="text-[#5E244E] dark:text-[#C97FAE]">Vi</span
-        ><span class="text-[#E68457]">Rent</span>
+        <span class="text-[#5E244E] dark:text-[#F3EEF5]">Vi</span
+        ><span class="text-[#E68457] dark:text-[#F3EEF5]">Rent</span>
     </span>
 </template>
